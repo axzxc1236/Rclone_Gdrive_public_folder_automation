4c1a43f8a9d56226c49c335cca7cdaf2c4c31de5
axzxc1236 <axzxc1236@gmail.com>
2019-06-10 23:01:55 +0800

    Upload the good stuff.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Rclone_Gdrive_public_folder_automation
+# Rclone Gdrive public folder automation
+
+## Why this is created
+I need to copy many publically shared Google Drive folder with rclone, you might know there is a copy function on Google Drive webpage, why not use that?
+1. I might want to copy these folders to my computer.
+2. I might want to copy these folders to my Google Drive but encrypted.
+3. I might want to copy these folders to another cloud storage providers. (for example Dropbox, FTP Server...)
+
+## How to use it?
+1. Install [Node.js](https://nodejs.org/en/download/)
+2. replace rclone.conf file with working one. (use "rclone config file" command to find where it's located.)
+3. Make sure rclone.conf starts with a drive remote, or at least conrains a drive remote, with a working token.
+4. modify filelist.txt with following format
+> (public folder ID)===(destination)
+
+> for example [https://drive.google.com/drive/folders/1N7rmP_1y4eo8bc75muJQPHXss-GgR1ja](https://drive.google.com/drive/folders/1N7rmP_1y4eo8bc75muJQPHXss-GgR1ja) has the folder ID 1N7rmP_1y4eo8bc75muJQPHXss-GgR1ja
+> 
+> (If you are wondering what is the file I linked, it's [a known troll file](https://www.reddit.com/r/DHExchange/comments/ax4or0/psa_beware_of_this_individual_who_claims_to_have/) contains all the Rick rolls you need.)
+
+> destination can be whatever file path you put into rclone, like "encrypted:Rick roll troll" (encrypted remote must be in rclone.conf you provided for that to work) or just "Rick roll troll", **It doesn't need to have double quotes even if the path contains whitespace** (at least on Windows 10), it looks like Node.JS adds double quotes itself.
+
+5. After you configured correctly, run "node app.js" and see if it works.
+
+## Note
+
+If you don't have rclone(.exe) in your path environment variable, you need to change "rcloneCommand" in app.js to something like "C:/rclone/rclone.exe" or wherever rclone is located.
---
+++ app.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const fs = require("fs");
+const { spawn } = require('child_process');
+const { EOL } = require('os');
+
+const filelist = "filelist.txt"
+const original_config = "rclone.conf";
+const modified_config = "rclone_modified.conf";
+const rcloneCommand = "rclone"; //If you are using Windows and rclone.exe is not in path, you want to change that to something like "C:/rclone/rclone.exe"
+
+function readToken() {
+	let token = null;
+	try {
+		let configContent = fs.readFileSync(original_config, { encoding: "utf-8" }).split(/\r?\n/);
+		for (let i = 0; i < configContent.length; i++) {
+			if (configContent[i].startsWith("token = ")) {
+				token = configContent[i].replace("token = ", "");
+				console.log("token=  " + token);
+			}
+		}
+	} catch (e) {
+		console.log("encountered error trying to read token:  " + e);
+	}
+	return token;
+}
+
+var folderIDList = [];
+var destinationList = [];
+var errorParsingFileList = false;
+function readFileList() {
+	try {
+		let fileListContent = fs.readFileSync(filelist, { encoding: "utf-8" }).split(/\r?\n/);
+		for (let i = 0; i < fileListContent.length; i++) {
+			if (fileListContent[i] != "") {
+				let array = fileListContent[i].split("===");
+				if (array.length != 2) {
+					console.log("error parsing this line: " + fileListContent[i]);
+					errorParsingFileList = true;
+				} else {
+					folderIDList.push(array[0]);
+					destinationList.push(array[1]);
+				}
+			}
+		}
+	} catch (e) {
+		console.log("encountered error trying to read filelist:  " + e);
+		errorParsingFileList = true;
+	}
+}
+
+function downloadFile(index) {
+	//copy a config file with different folder ID
+	if (fs.existsSync(modified_config))
+		fs.unlinkSync(modified_config)
+	fs.copyFileSync(original_config, modified_config);
+	fs.appendFileSync(modified_config,
+		EOL + "[tmp]" + EOL +
+		"type = drive" + EOL +
+		"scope = drive" + EOL +
+		"root_folder_id = " + folderIDList[index] + EOL +
+		"token = " + token + EOL);
+	//
+
+	//spawn rclone process
+	const rclone = spawn(rcloneCommand, ["--config", modified_config, "-P", "copy", "tmp:", destinationList[index]], { stdio: 'inherit' });
+
+	rclone.on('close', (code) => {
+		console.log("child process exited with code" + code);
+		if (code != 0) {
+			console.log("encountered an error, not continuing.");
+		} else if (index == folderIDList.length) {
+			console.log("I think we are done.");
+		} else {
+			downloadFile(index++);
+		}
+	});
+	//
+}
+
+var token = readToken();
+if (token != null) {
+	readFileList();
+	if (!errorParsingFileList && folderIDList.length != 0) {
+		console.log("Good to go.");
+		downloadFile(0);
+	}
+}
