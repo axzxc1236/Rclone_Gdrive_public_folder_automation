103870f7d4311ebe33f2316b274a980a7a4bd9d6
axzxc1236 <axzxc1236@gmail.com>
2019-06-11 16:19:07 +0800

    An attempt to save renewed token. (not tested)

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const filelist = "filelist.txt"
 const original_config = "rclone.conf";
 const modified_config = "rclone_modified.conf";
 const rcloneCommand = "rclone"; //If you are using Windows and rclone.exe is not in path, you want to change that to something like "C:/rclone/rclone.exe"
+const writeDebugFile = true;
+const debug_file_filename = "debug_file";
 
 function readTokenFromOriginalConfig() {
 	let token = null;
@@ -17,6 +19,10 @@ function readTokenFromOriginalConfig() {
 			if (configContent[i].startsWith("token = ")) {
 				token = configContent[i].replace("token = ", "");
 				console.log("token=  " + token);
+				//Debug
+				if (writeDebugFile)
+					fs.appendFileSync(debug_file_filename, Date() + "  " + token + EOL);
+				//
 			}
 		}
 	} catch (e) {
@@ -73,10 +79,44 @@ function downloadFile(index) {
 		} else if (index == folderIDList.length) {
 			console.log("I think we are done.");
 		} else {
+			readTokensFromModifiedConfig();
 			downloadFile(index+1);
 		}
 	});
 	//
+}
+
+function readTokensFromModifiedConfig() {
+	//This function is used to find renewed token saved in modified config.
+	//When rclone is running, it might renew a new token and save it in modified_config
+	//, but modified_config is going to be replaced with content from original_config
+	//, so we need to find and save the new token.
+	try {
+		let modifiedConfigContent = fs.readFileSync(modified_config, { encoding: "utf-8" }).split(/\r?\n/);
+		for (let i = 0; i < modifiedConfigContent.length; i++) {
+			if (modifiedConfigContent[i].startsWith("token = ")) {
+				let tmp_token = modifiedConfigContent[i].replace("token = ", "");
+				if (token != tmp_token) {
+					console.log("I found a new token: " + tmp_token);
+					console.log("old one is: " + token);
+					//Debug
+					if (writeDebugFile)
+						fs.appendFileSync(debug_file_filename,
+							Date() + "  old token is " + token + EOL +
+							Date() + "  new token is " + tmp_token + EOL);
+					//
+					//Modify original config to replace token with new one.
+					let originalConfigFileContent = fs.readFileSync(original_config, { encoding: "utf-8" });
+					originalConfigFileContent = originalConfigFileContent.replace(token, tmp_token);
+					fs.writeFileSync(original_config, originalConfigFileContent);
+					//
+					token = tmp_token;
+				}
+			}
+		}
+	} catch (e) {
+		console.log("encountered error trying to read token:  " + e);
+	}
 }
 
 var token = readTokenFromOriginalConfig();
